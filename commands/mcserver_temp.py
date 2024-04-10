da02bbb113d8e1425e65d007886083b93664a247
thoni08 <88326103+thoni08@users.noreply.github.com>
2024-04-10 23:00:06 +0700

    add start server command for exaroton before turning to self host

---
+++ commands/mcserver_temp.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+from exaroton import Exaroton
+from os.path import join, dirname
+
+load_dotenv(join(dirname(__file__), '.env'))
+exaroton = Exaroton(os.getenv('MCTOKEN'))
+
+class McServer(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{os.path.basename(__file__)} ready.")
+
+    @discord.app_commands.command(name="start", description="Start Minecraft Exaroton server.")
+    async def start(self, interaction: discord.Interaction) -> None:
+        exaroton.start(os.getenv('MCSERVER_ID'))
+        await interaction.response.send_message("Server starting.")
+
+async def setup(bot):
+    await bot.add_cog(McServer(bot))
